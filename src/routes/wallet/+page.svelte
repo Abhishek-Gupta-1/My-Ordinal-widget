7996fadce3da7b04521f8d4243c01e232a2ccf83
nagaumamahesh <nandi.umamahesh.2003@gmail.com>
2023-08-06 18:19:49 +0530

    Wallet Page functionality implemented

--- src/routes/wallet/+page.svelte
+++ src/routes/wallet/+page.svelte
@@ -1,132 +1,119 @@
 <script>
-    import { onMount } from "svelte";
-    import { getAuthToken } from "../../services/authService";
+  import { onMount } from 'svelte';
+  import { getAuthToken } from '../../services/authService';
 
-    /**
-     * @type {string | null}
-     */
-    let accessToken;
-    /**
-     * @type {any}
-     */
-    let walletId;
-    let balance = {
-      confirmed: 0,
-      unconfirmed: 0,
-      summary: 0,
-      count: 0,
-    };
-    /**
-     * @type {string | any[]}
-     */
-    let addresses = [];
-    /**
-     * @type {string}
-     */
-    let generatedAddress = "Click 'Generate Address' to get a random address";
+  let accessToken;
+  $: balance = {
+    confirmed: 0,
+    unconfirmed: 0,
+    summary: 0,
+    count: 0,
+  };
 
-  
-    async function getWalletInfo() {
-      try {
-        // Get the access token
-        accessToken = await getAuthToken();
-  
-        // Call API to get wallet IDs
-        const walletListResponse = await fetch("https://api.neucron.io/custom/wallet/list", {
-          // @ts-ignore
-          headers: {
-            Authorization: accessToken,
-          },
-        });
-        const walletListData = await walletListResponse.json();
-        walletId = walletListData.data.walletList[0];
-        console.log(walletId)
-  
-        // Call API to get wallet balance
-        const balanceResponse = await fetch(
-          `https://api.neucron.io/custom/wallet/balance?walletID=${walletId}`,
-          {
-            // @ts-ignore
-            headers: {
-              Authorization: accessToken,
-            },
-          }
-        );
-        const balanceData = await balanceResponse.json();
-        balance = balanceData.data.balance;
-  
-        // Set initial generated address
-      } catch (error) {
-        console.error("Error occurred:", error);
-      }
+  let generatedAddress = '';
+  let walletId = null;
+
+  async function getWalletList() {
+    try {
+      accessToken = await getAuthToken();
+      const response = await fetch('https://api.neucron.io/custom/wallet/list', {
+        headers: {
+          Authorization: accessToken,
+        },
+      });
+      const data = await response.json();
+      const walletList = data.data.walletList;
+      walletId = walletList[0]; // Get the first walletId from the list
+    } catch (error) {
+      console.error('Error occurred:', error);
     }
-  
-    async function generateAddress() {
-      try {
-        // Call API to get addresses
-        const addressResponse = await fetch(
-          `https://api.neucron.io/custom/wallet/address?walletID=${walletId}`,
-          {
-            // @ts-ignore
-            headers: {
-              Authorization: accessToken,
-            },
-          }
-        );
-        const addressData = await addressResponse.json();
-        addresses = addressData.data.addresses;
-  
-        // Select a random address from the list
-        generatedAddress =
-          addresses[Math.floor(Math.random() * addresses.length)];
-      } catch (error) {
-        console.error("Error occurred:", error);
-      }
+  }
 
+  async function getBalance() {
+    try {
+      if (!walletId) {
+        await getWalletList(); // Fetch the walletId if not available
+      }
+      const response = await fetch(`https://api.neucron.io/custom/wallet/balance?walletID=${walletId}`, {
+        headers: {
+          Authorization: accessToken,
+        },
+      });
+      const data = await response.json();
+      balance = data.data.balance;
+    } catch (error) {
+      console.error('Error occurred:', error);
     }
-    
-    // onMount(async () => {
-      
-    // });
-  </script>
-  
-  <main>
-    <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-      <div class="mx-auto max-w-lg">
-        <h1 class="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">
-          Wallet Details
-        </h1>
-  
-        <div class="mt-6 p-4 rounded-lg border border-gray-300 shadow-lg">
-          <p class="text-lg font-medium">Wallet ID: {walletId}</p>
-          <p class="text-lg font-medium">Confirmed Balance: {balance.confirmed}</p>
-          <p class="text-lg font-medium">Unconfirmed Balance: {balance.unconfirmed}</p>
-          <p class="text-lg font-medium">Summary: {balance.summary}</p>
-          <p class="text-lg font-medium">Transaction Count: {balance.count}</p>
-        </div>
-  
-        <div class="mt-6">
-          <p class="text-lg font-medium">Addresses:</p>
-          {#each addresses as address}
-            <p>{address}</p>
-          {/each}
-        </div>
-  
-        <div class="mt-6">
-          <button
-            class="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
-            on:click={generateAddress}
-          >
-            Generate Address
-          </button>
-        </div>
-  
-        
-        <div class="mt-6">
-          <p class="text-lg font-medium">Generated Address:</p>
-          <p>{generatedAddress}</p>
-        </div>
+  }
+
+  async function generateAddress() {
+    try {
+      if (!walletId) {
+        await getWalletList(); // Fetch the walletId if not available
+      }
+      const response = await fetch(`https://api.neucron.io/custom/wallet/address?walletID=${walletId}`, {
+        headers: {
+          Authorization: accessToken,
+        },
+      });
+      const data = await response.json();
+      generatedAddress = data.data.addresses[0];
+    } catch (error) {
+      console.error('Error occurred:', error);
+    }
+  }
+
+  onMount(async () => {
+    await getBalance();
+  });
+</script>
+
+<main style="width: 90%; margin:auto; margin-top: 16px; height: 75vh;">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <div class="p-4 bg-white rounded-lg shadow-md">
+    <div class="flex items-center justify-between">
+      <div>
+        <h3 class="text-lg font-medium mb-2">Current Balance</h3>
+        <p class="text-4xl font-bold text-indigo-600">{balance.confirmed}</p>
       </div>
+      <button class="text-gray-500 hover:text-indigo-600 transition" on:click={getBalance}>
+        <span class="material-symbols-outlined">
+          refresh
+          </span>
+      </button>
     </div>
-  </main>
-  
+
+    <div class="mt-4">
+      <p class="text-gray-600">Unconfirmed: {balance.unconfirmed}</p>
+      <p class="text-gray-600">Transactions Count: {balance.count}</p>
+    </div>
+
+    <div class="mt-6">
+      <button class="block w-full rounded-lg bg-indigo-600 px-4 py-2 text-sm font-medium text-white" on:click={generateAddress}>
+        Get Fundable Address
+      </button>
+    </div>
+
+    {#if generatedAddress}
+    <div class="mt-6">
+      <p class="text-gray-600">Generated Address: {generatedAddress}</p>
+    </div>
+    {/if}
+
+    <div class="mt-4">
+      <a href="https://relysia.com/" target="_blank" class="text-indigo-600 hover:underline">
+        Fund Using Relysia
+      </a>
+    </div>
+  </div>
+</main>
+
+<style>
+  .material-symbols-outlined {
+    font-variation-settings:
+    'FILL' 0,
+    'wght' 400,
+    'GRAD' 0,
+    'opsz' 48
+  }
+</style>
