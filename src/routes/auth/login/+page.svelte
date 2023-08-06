3ab5286593e32854d06050726b10a5d1c8c94562
Naga Uma Mahesh <114081906+nagaumamahesh@users.noreply.github.com>
2023-08-06 12:21:14 +0530

    Add files via upload

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,21 +1,21 @@
 <script>
-  import { goto } from '$app/navigation';
-  import { setAuthToken } from '../../../services/authService';
-  import { setName } from '../../../services/userDetails';
+  import { goto } from "$app/navigation";
+  import { setAuthToken } from "../../../services/authService";
+  import { setName } from "../../../services/userDetails";
 
-  let email = '';
-  let password = '';
+  let email = "";
+  let password = "";
   let showPassword = false;
-  let errorMessage = '';
+  let errorMessage = "";
 
   async function handleSubmit(event) {
     event.preventDefault();
 
     try {
-      const response = await fetch('https://api.neucron.io/auth/login', {
-        method: 'POST',
+      const response = await fetch("https://api.neucron.io/auth/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ email, password }),
       });
@@ -25,10 +25,12 @@
       if (response.ok) {
         handleSuccessfulLogin(data.data.access_token);
       } else {
-        handleLoginError(data.message || 'Login failed. Please try again.');
+        handleLoginError(data.message || "Login failed. Please try again.");
       }
     } catch (error) {
-      handleLoginError('An error occurred during login. Please try again later.');
+      handleLoginError(
+        "An error occurred during login. Please try again later."
+      );
       console.error(error);
     }
   }
@@ -36,9 +38,9 @@
   function handleSuccessfulLogin(token) {
     setAuthToken(token);
     setName(email);
-    goto('/dashboard');
-    console.log('Login successful');
-    resetForm();
+    goto("/dashboard");
+    // console.log("Login successful");
+    // resetForm();
   }
 
   function handleLoginError(message) {
@@ -46,107 +48,44 @@
   }
 
   function resetForm() {
-    email = '';
-    password = '';
+    email = "";
+    password = "";
     showPassword = false;
   }
 </script>
 
-<div class="min-h-screen flex items-center justify-center">
-  <div class="max-w-md w-full">
-    <div class="bg-white py-8 px-6 rounded-lg shadow-md">
-      <h3 class="mb-4 text-xl font-medium text-gray-900">Sign in to our platform</h3>
-      <form class="space-y-4" on:submit={handleSubmit}>
-        <div>
-          <label for="email" class="block mb-1 font-medium text-gray-700">Email</label>
-          <input
-                  type="email"
-                  id="email"
-                  bind:value={email}
-                  placeholder="name@company.com"
-                  required
-                  class="input-field"
-          />
+<div style="min-height: 100vh; display: flex; align-items: center; justify-content: center;">
+  <div style="max-width: 28rem; width: 100%;">
+    <div style="background-color: white; padding: 2rem; border-radius: 0.5rem; box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);">
+      <h3 style="margin-bottom: 1rem; font-size: 1.5rem; font-weight: 500; color:indigo;">Login into yout account</h3>
+      <form on:submit={handleSubmit} style="margin-bottom: 1rem;">
+        <div style="margin-bottom: 1rem;">
+          <label for="email" style="display: block; margin-bottom: 0.25rem; font-weight: 500; color: #4B5563;">Email</label>
+          <input type="email" id="email" bind:value={email} placeholder="Enter your mail" required style="width: 100%; padding: 0.5rem; border: 1px solid #ccc; border-radius: 0.25rem; outline: none; transition: border-color 0.2s;" />
         </div>
-        <div>
-          <label for="password" class="block mb-1 font-medium text-gray-700">Password</label>
+        <div style="margin-bottom: 1rem;">
+          <label for="password" style="display: block; margin-bottom: 0.25rem; font-weight: 500; color: #4B5563;">Password</label>
           {#if showPassword}
-            <input
-                    type="text"
-                    id="password"
-                    bind:value={password}
-                    placeholder=""
-                    required
-                    class="input-field"
-            />
+            <input type="text" id="password" bind:value={password} placeholder="" required style="width: 100%; padding: 0.5rem; border: 1px solid #ccc; border-radius: 0.25rem; outline: none; transition: border-color 0.2s;" />
           {:else}
-            <input
-                    type="password"
-                    id="password"
-                    bind:value={password}
-                    placeholder=""
-                    required
-                    class="input-field"
-            />
+            <input type="password" id="password" bind:value={password} placeholder="" required style="width: 100%; padding: 0.5rem; border: 1px solid #ccc; border-radius: 0.25rem; outline: none; transition: border-color 0.2s;" />
           {/if}
         </div>
-        <div class="flex items-center">
-          <input
-                  type="checkbox"
-                  class="form-checkbox text-orange-500"
-                  bind:checked={showPassword}
-          />
-          <span
-                  class="ml-2 text-gray-700 cursor-pointer"
-                  on:click={() => (showPassword = !showPassword)}
-          >
-            Show password
-          </span>
-          <a href="/auth/forgotpassword" class="ml-auto text-sm text-orange-500 hover:underline">
-            Lost password?
-          </a>
+        <div style="display: flex; align-items: center;">
+          <input type="checkbox" class="form-checkbox text-indigo-900" bind:checked={showPassword} style="margin-right: 0.5rem;" />
+          <span style="color: indigo; cursor: pointer;" on:click={() => (showPassword = !showPassword)}>Show password</span>
+          <a href="/auth/forgotpassword" style="margin-left: auto; font-size: 0.875rem; color: indigo; cursor: pointer;">Lost password?</a>
         </div>
         {#if errorMessage}
-          <p class="text-red-500 text-sm">{errorMessage}</p>
+          <p style="color: #DC2626; font-size: 0.875rem; margin-top: 0.5rem;">{errorMessage}</p>
         {/if}
-        <button
-                type="submit"
-                class="button-primary"
-        >
+        <button type="submit" style="width: 100%; margin-top :10px;background-color: indigo; color: white; font-weight: 500; padding: 0.5rem; border: none; border-radius: 0.25rem; cursor: pointer; transition: background-color 0.2s;">
           Login to your account
         </button>
-        <div class="text-sm text-gray-500 mt-2">
-          Not registered? <a href="/auth/signup" class="text-orange-500 hover:underline">Create account</a>
+        <div style="margin-top: 0.5rem; font-size: 0.875rem; color: black;">
+          Not registered? <a href="/auth/signup" style="color: indigo; cursor: pointer; text-decoration: underline;">Create account</a>
         </div>
       </form>
     </div>
   </div>
 </div>
-
-<style>
-  .input-field {
-    width: 100%;
-    padding: 0.5rem;
-    border: 1px solid #ccc;
-    border-radius: 0.25rem;
-    outline: none;
-    transition: border-color 0.2s;
-  }
-  .input-field:focus {
-    border-color: #f59e0b;
-  }
-  .button-primary {
-    width: 100%;
-    background-color: #f59e0b;
-    color: white;
-    font-weight: 500;
-    padding: 0.5rem;
-    border: none;
-    border-radius: 0.25rem;
-    cursor: pointer;
-    transition: background-color 0.2s;
-  }
-  .button-primary:hover {
-    background-color: #e8871d;
-  }
-</style>
