1f47a6990415916fa7e38c082ba2f665071b7516
Abhishek Gupta <109204269+Abhishek-Gupta-1@users.noreply.github.com>
2023-08-09 15:28:17 +0530

    Created Documentation Page
    Added Content to the Documentation Page. Need Further Update and Formatting.

---
+++ src/routes/docs/+page.svelte
@@ -0,0 +1,93 @@
+<script>
+    import { P } from "flowbite-svelte";
+
+</script>
+
+  <main>
+    <!-- <div class="mx-auto max-w-screen-xxl px-4 py-16 sm:px-6 lg:px-8"> -->
+      <!-- <div class="mx-auto max-w-xl"> -->
+
+        <div style="padding-left: 18rem; padding-right: 6rem;" >
+        <h1 class="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">
+            <br>
+            <br>
+          <strong style="text-align:center; padding-right: 7rem;">Mint Ordinal Documentation</strong>
+          <br>
+          
+          <br>
+        </h1>
+        <p>Welcome to the Mint Ordinal, where you can easily mint your own unique NFTs. 
+           This documentation  will guide you through the <br> step-by-step  process of wallet creation, wallet funding, and minting NFTs using Bitcoin SV.</p>
+           <br>
+           <br>
+           
+<h1><strong>1. Wallet Creation</strong></h1>
+<br>
+<p>
+To get started, follow these steps to create your wallet: <br>
+<br>
+1. Visit our website and navigate to the "Wallet" section. <br>
+2. Click on the "Create Wallet" button. <br>
+3. A new wallet will be generated for you. It's crucial to save your secret key and mnemonics in a secure place. <br>
+ These details are essential for wallet recovery and transaction signing. Do not share them with anyone and ensure
+  they are stored offline. <br>
+</p>
+<br>
+<h1><strong>2. Retrieving Wallet ID:</strong></h1>
+<br>
+<p>
+
+After wallet creation, you will be assigned a unique Wallet ID. 
+This ID is required to access your wallet and perform various actions. <br>
+You can find your Wallet ID on your dashboard after logging in. <br>
+</p>
+<br>
+<h1><strong> 3. Retrieving Wallet Address:</strong></h1>
+<br>
+<p>
+
+To retrieve your wallet address, follow these steps:
+<br>
+<br>
+1. Go to the "Wallet" section. <br>
+2. Your wallet address will be displayed on this page. This is the address you'll need to fund with BSV Token in order to make transaction(Minting). <br>
+</p>
+<br>
+ 
+<h1><strong>4. Funding Your Wallet:</strong></h1> <br>
+<p>
+    
+To fund your wallet with BSV Coin, follow these instructions: <br>
+<br>
+1. Obtain a small amount of Bitcoin SV (some satoshi approx. 0.00009$ BSV is enough to make multiple transaction(Minting) ) from your preferred cryptocurrency exchange or wallet. <br>
+2. Use your wallet address to receive the Bitcoin SV (or BSV). You can copy and paste the address or scan the provided QR code. <br>
+3. Once the transaction is confirmed on the blockchain, your wallet will be funded and ready for NFT minting. <br>
+</p>
+
+<br>
+<h1><strong>5. Minting Image or Text:</strong></h1> <br>
+<p>
+You're now ready to mint your own NFTs! Here's how you can do it:  
+<br>
+<br>
+1. Click on "Mint" button in the Naviagtion bar. <br>
+2. Choose between minting an image or text-based NFT. <br>
+<br>
+   - For images: Upload the image you want to tokenize. You can provide a title, description, and any other relevant information. <br>
+   - For text-based NFTs: Enter your desired text in the provided textbox. You can also add additional details. <br> <br>
+3. Review the information and confirm the minting process. <br>
+4. Once the minting process is complete, your NFT will be created and associated with your wallet. <br>
+<br>
+Congratulations! You have successfully created a wallet, funded it, and minted your own NFTs. <br> Remember that NFTs are unique digital assets, and you can manage and trade them as desired. <br>
+<br>
+If you have any further questions or need assistance, please feel free to reach out to our support team. Happy minting!
+
+<br>
+<br>
+<br>
+</p>
+
+</div>
+      
+  </main>
+  
